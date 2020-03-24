c54b93552a7a790a8dd68ffb2a4c394ed41d6df4
Admin <qinghongteo@gmail.com>
2020-03-25 01:03:28 +0800

    cleaned code, submenu working

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, callbackcontext
 from config import TOKEN
 
 # Enable logging
@@ -11,59 +11,74 @@ logger = logging.getLogger(__name__)
 
 
 def start(update, context):
-    # keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
-    #              InlineKeyboardButton("Option 2", callback_data='2')],
-    #             [InlineKeyboardButton("Option 3", callback_data='3')]]
-    #
-    # reply_markup = InlineKeyboardMarkup(keyboard)
-    #
-    # update.message.reply_text('Please choose:', reply_markup=reply_markup)
     update.message.reply_text(main_menu_message(),
                               reply_markup=main_menu_keyboard())
 
 
 def main_menu(update,context):
-    print(update.callback_query.data)
     query = update.callback_query
-    print(query)
-    context.edit_message_text(chat_id=query.message.chat_id,
+    context.bot.edit_message_text(chat_id=query.message.chat_id,
                                   message_id=query.message.message_id,
                                   text=main_menu_message(),
                                   reply_markup=main_menu_keyboard())
 
 
+def first_menu(update, context):
+    query = update.callback_query
+    # print(context.match)
+    context.bot.edit_message_text(chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id,
+                                  text=first_menu_message(),
+                                  reply_markup=first_menu_keyboard())
 
-# def first_menu(context, update):
+def second_menu(update, context):
+    query = update.callback_query
+    # print(context.match)
+    context.bot.edit_message_text(chat_id=query.message.chat_id,
+                                  message_id=query.message.message_id,
+                                  text=second_menu_message(),
+                                  reply_markup=second_menu_keyboard())
+
+
+# def second_menu(update, context):
 #     query = update.callback_query
-#     print(query)
-#     query.edit_message_text(chat_id=query.message.chat_id,
-#                                 message_id=query.message.message_id,
-#                                 text=first_menu_message(),
-#                                 reply_markup=first_menu_keyboard())
-#     print("hello")
-#
-# # and so on for every callback_data option
-# def first_submenu(bot, update):
-#     pass
-#
-#
-# def second_submenu(bot, update):
-#   pass
+#     context.bot.edit_message_text(chat_id=query.message.chat_id,
+#                                   message_id=query.message.message_id,
+#                                   text=second_menu_message(),
+#                                   reply_markup=second_menu_keyboard())
+
+
+# and so on for every callback_data option
+def first_submenu(bot, update):
+    pass
+
+
+def second_submenu(bot, update):
+    pass
 
 
 ################ Keyboard #######################
 def main_menu_keyboard():
-    keyboard = [[InlineKeyboardButton("Option 1", callback_data='1'),
+    keyboard = [[InlineKeyboardButton("Option 1", callback_data='m1'),
                  InlineKeyboardButton("Option 2", callback_data='m2')],
                 [InlineKeyboardButton("Option 3", callback_data='m3')]]
     return InlineKeyboardMarkup(keyboard)
 
 
-# def first_menu_keyboard():
-#     keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
-#               [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
-#               [InlineKeyboardButton('Main menu', callback_data='main')]]
-#     return InlineKeyboardMarkup(keyboard)
+def first_menu_keyboard():
+    keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
+              [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
+              [InlineKeyboardButton('Main menu', callback_data='main')]]
+    return InlineKeyboardMarkup(keyboard)
+
+
+def second_menu_keyboard():
+  keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
+              [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
+              [InlineKeyboardButton('Main menu', callback_data='main')]]
+  return InlineKeyboardMarkup(keyboard)
+
+
 
 
 ####### Messages ############
@@ -75,19 +90,10 @@ def first_menu_message():
     return 'Choose the submenu in first menu:'
 
 
-def button(update, context):
-    query = update.callback_query
-    print(query)
-    # name = context
-    print(update.callback_query.message.chat.first_name)
-    if query.data == "1":
-        menu_1 = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
-                  [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')]]
-        reply_markup = InlineKeyboardMarkup(menu_1)
-        context.bot.edit_message_text(chat_id=query.message.chat_id,
-                                      message_id=query.message.message_id,
-                                      text='Choose the option:',
-                                      reply_markup=reply_markup)
+def second_menu_message():
+    return 'Choose the submenu in second menu:'
+
+######## Other commands ###############
 
 
 def help(update, context):
@@ -103,10 +109,13 @@ def main():
 
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
-    # updater.dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
-    # updater.dispatcher.add_handler(CallbackQueryHandler(first_submenu,
-    #                                                     pattern='m1_1'))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.dispatcher.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
+
+    updater.dispatcher.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
+
+    updater.dispatcher.add_handler(CallbackQueryHandler(first_submenu, pattern='m1_1'))
+    updater.dispatcher.add_handler(CallbackQueryHandler(second_submenu, pattern='m2_1'))
+
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.dispatcher.add_error_handler(error)
 
